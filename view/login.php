e7ad72b8ddd7c9125a73e3fe540cbd62f36285db
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-03 19:27:54 +0330

    login view

---
+++ view/login.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../controller/DBConnection.php';
+require_once '../controller/UserConnection.php';
+
+$db = new DBConnection();
+$uc = new UserConnection($_POST, $db);
+
+$uc->checkLoginParams();
+$uc->checkLogin();
