db63135acd14091f5718ca719e0fa10e4879c418
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-07 14:00:51 +0330

    changed insert api for uploading images

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -30,36 +30,36 @@ class Api {
   }
 
   static Future<bool> insert(
-      String productName,
-      String productType,
-      String productSubtype,
-      String price,
-      String minSize,
-      String maxSize,
-      String colors,
-      String image,
-      String description) async {
+    String productName,
+    String productType,
+    String productSubtype,
+    String price,
+    String minSize,
+    String maxSize,
+    String colors,
+    String image,
+    String description,
+  ) async {
     Uri url = Uri.parse('http://192.168.102.216/NBB/view/product.php');
-    Response response = await post(
-      url,
-      body: {
-        'apiType': 'insert',
-        'product_name': productName,
-        'product_type': productType,
-        'product_subtype': productSubtype,
-        'price': price,
-        'min_size': minSize,
-        'max_size': maxSize,
-        'colors': colors,
-        'image': image,
-        'description': description,
-        'userId': '20',
-        'password': 'sadra'
-      },
-    );
+    var request = MultipartRequest('POST', url)
+      ..fields['apiType'] = 'insert'
+      ..fields['product_name'] = productName
+      ..fields['product_type'] = productType
+      ..fields['product_subtype'] = productSubtype
+      ..fields['price'] = price
+      ..fields['min_size'] = minSize
+      ..fields['max_size'] = maxSize
+      ..fields['colors'] = colors
+      ..fields['image'] = image
+      ..fields['description'] = description
+      ..fields['userId'] = '20'
+      ..fields['password'] = 'sadra';
+    //..files.add(await MultipartFile.fromPath('image', image));
+
+    StreamedResponse response = await request.send();
 
     if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
+      Map<String, dynamic> data = jsonDecode(await response.stream.bytesToString());
       if (data['result'] == true) return true;
       if (data['result'] == false) return false;
     }
