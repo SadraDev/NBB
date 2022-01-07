ad35867dc8dcd082ac4cf012e9676701083ea8ba
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-07 17:07:15 +0330

    changed clothing to cloth and added logic

---
+++ lib/screens/cloth.dart
@@ -0,0 +1,171 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:nbb/const.dart';
+import 'package:nbb/models/productModel.dart';
+import 'package:nbb/utils/api.dart';
+import 'package:nbb/widgets/shoe_cloth_widgets/grids.dart';
+import 'package:nbb/widgets/shoe_cloth_widgets/modalBottomSheet.dart';
+import 'package:nbb/widgets/shoe_cloth_widgets/subTypeSelector.dart';
+
+class ClothScreen extends StatefulWidget {
+  const ClothScreen({Key? key}) : super(key: key);
+
+  @override
+  _ClothScreenState createState() => _ClothScreenState();
+}
+
+class _ClothScreenState extends State<ClothScreen> {
+  List<Product> products = [];
+
+  Future<void> getProducts() async {
+    List<dynamic> products = await Api.selectAllProducts();
+    setState(() {
+      for (var product in products) {
+        Product newProduct = Product(
+          productName: product[1],
+          productType: product[2],
+          productSubtype: product[3],
+          minSize: product[4],
+          maxSize: product[5],
+          price: product[6],
+          colors: jsonDecode(product[7]),
+          image: product[8],
+          description: product[9],
+          deleted: product[10],
+        );
+
+        if (subType1! && newProduct.productSubtype == 't-shirt') {
+          this.products.add(newProduct);
+        }
+        if (subType2! && newProduct.productSubtype == 'pants') {
+          this.products.add(newProduct);
+        }
+        if (subType3! && newProduct.productSubtype == 'sets') {
+          this.products.add(newProduct);
+        }
+        if (subType3! && newProduct.productSubtype == 'coats') {
+          this.products.add(newProduct);
+        }
+        if (subType4! && newProduct.productSubtype == 'skirts') {
+          this.products.add(newProduct);
+        }
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProducts();
+  }
+
+  bool? subType1 = true;
+  bool? subType2 = false;
+  bool? subType3 = false;
+  bool? subType4 = false;
+  bool? subType5 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SubTypeSelector(
+          subtype1Name: 't-shirt',
+          subtype1Color: subType1! ? Colors.white : greyColor,
+          onTapForSubtype1: () => setState(() {
+            subType1 = true;
+            subType2 = false;
+            subType3 = false;
+            subType4 = false;
+            subType5 = false;
+            products = [];
+            getProducts();
+          }),
+          subtype2Name: 'pants',
+          subtype2Color: subType2! ? Colors.white : greyColor,
+          onTapForSubtype2: () => setState(() {
+            subType1 = false;
+            subType2 = true;
+            subType3 = false;
+            subType4 = false;
+            subType5 = false;
+            products = [];
+            getProducts();
+          }),
+          subtype3Name: 'sets',
+          subtype3Color: subType3! ? Colors.white : greyColor,
+          onTapForSubtype3: () => setState(() {
+            subType1 = false;
+            subType2 = false;
+            subType3 = true;
+            subType4 = false;
+            subType5 = false;
+            products = [];
+            getProducts();
+          }),
+          subtype4Name: 'coats',
+          subtype4Color: subType4! ? Colors.white : greyColor,
+          onTapForSubtype4: () => setState(() {
+            subType1 = false;
+            subType2 = false;
+            subType3 = false;
+            subType4 = true;
+            subType5 = false;
+            products = [];
+            getProducts();
+          }),
+          subtype5Name: 'skirts',
+          subtype5Color: subType5! ? Colors.white : greyColor,
+          onTapForSubtype5: () => setState(() {
+            subType1 = false;
+            subType2 = false;
+            subType3 = false;
+            subType4 = false;
+            subType5 = true;
+            products = [];
+            getProducts();
+          }),
+        ),
+        ShoeAndClothGridView(
+          count: products.length,
+          builder: (context, index) {
+            bool? pink = products[index].colors!['pink'];
+            bool? blue = products[index].colors!['blue'];
+            bool? green = products[index].colors!['green'];
+            bool? red = products[index].colors!['red'];
+            bool? black = products[index].colors!['black'];
+            if (products[index].productType == 'Cloth') {
+              return ShoeAndClothGrid(
+                name: products[index].productName,
+                image: products[index].image,
+                price: products[index].price,
+                onLiked: () {},
+                modalBuilder: (context) {
+                  return ModalBottomSheetForShoeAndCloth(
+                    name: products[index].productName,
+                    subtype: products[index].productSubtype,
+                    image: products[index].image,
+                    price: products[index].price,
+                    minSize: products[index].minSize,
+                    maxSize: products[index].maxSize,
+                    description: products[index].description,
+                    isShoe: products[index].productType == 'Shoe',
+                    pink: pink,
+                    blue: blue,
+                    green: green,
+                    red: red,
+                    black: black,
+                    onLiked: () {},
+                    onBuy: () {},
+                  );
+                },
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
