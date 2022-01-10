1538fa43f7e46a92896991e4a92be10cacf04bbf
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-11 02:30:10 +0330

    added update username view

---
+++ view/updateusername.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../controller/DBConnection.php';
+require_once '../controller/UserConnection.php';
+
+$db = new DBConnection();
+$uc = new UserConnection($_POST, $db);
+
+$uc->updateUsername();
