d14fc16d2ab8bacbd964e34a649264bd1a43d7c9
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-11 18:55:55 +0330

    added buy to products view

--- view/product.php
+++ view/product.php
@@ -6,15 +6,16 @@ require_once '../controller/UserConnection.php';
 $db = new DBConnection();
 $uc = new UserConnection($_POST, $db);
 
-if($uc->checkAuth()){
+if ($uc->checkAuth()) {
     require_once '../controller/ProductsController.php';
     $pc = new ProductsController($_POST, $db);
 
-    if($pc->checkProductParams()){
-        if(@$_POST['apiType'] == 'insert') $pc->insertProduct();
-        if(@$_POST['apiType'] == 'select_all') $pc->selectAllProducts();
-        if(@$_POST['apiType'] == 'select_by_type') $pc->selectProductsByType();
-        if(@$_POST['apiType'] == 'select_by_subType') $pc->selectProductBySubtype();
-        if(@$_POST['apiType'] == 'select_single') $pc->selectSingleProduct();
+    if ($pc->checkProductParams()) {
+        if (@$_POST['apiType'] == 'insert') $pc->insertProduct();
+        if (@$_POST['apiType'] == 'buy') $pc->onBuy();
+        if (@$_POST['apiType'] == 'select_all') $pc->selectAllProducts();
+        if (@$_POST['apiType'] == 'select_by_type') $pc->selectProductsByType();
+        if (@$_POST['apiType'] == 'select_by_subType') $pc->selectProductBySubtype();
+        if (@$_POST['apiType'] == 'select_single') $pc->selectSingleProduct();
     }
 }
