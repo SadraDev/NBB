c2e11e0057e3a87e4563586d7c1dde52c427adff
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-12 00:03:19 +0330

    profile screen logic separated from ui

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:nbb/screens/admin.dart';
-import '../const.dart';
+import 'package:nbb/utils/api.dart';
+import 'package:nbb/utils/shared.dart';
+import 'package:nbb/widgets/profile_widgets/profileBoughtProductBubble.dart';
+import 'package:nbb/widgets/profile_widgets/profileCenterBubble.dart';
+import 'package:nbb/widgets/profile_widgets/profileDynamicHeader.dart';
+import 'package:nbb/widgets/profile_widgets/profileUsernameEditor.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -10,108 +14,35 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  String? username;
+  List<dynamic> boughtProducts = [];
+
+  Future<void> getBoughtProducts() async {
+    List<dynamic> boughtProducts = await Api.getBoughtProducts();
+    setState(() {
+      this.boughtProducts = boughtProducts;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBoughtProducts();
+    username = Shared.getUserName();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      physics: const BouncingScrollPhysics(),
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 35).copyWith(bottom: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              const Text(
-                'Profile',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              TextButton(
-                child: const Text(
-                  ' Admin\nSettings',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                onPressed: () => Navigator.pushNamed(context, AdminScreen.id),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Center(
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    spreadRadius: 2,
-                    color: Colors.grey,
-                    offset: Offset(0, 10),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: const <Widget>[
-                  Text(
-                    'Total',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 8),
-                    child: Text(
-                      '2',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Products bought',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Jhon Doe',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Type to edit',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
+    return Builder(builder: (context) {
+      List<Widget> children = [
+        const ProfileDynamicHeader(),
+        ProfileScreenCenterBubble(count: boughtProducts.length.toString()),
+        ProfileScreenUsernameEditor(
+          username: username,
+          onSubmitted: (value) async {
+            Shared.setUserName(value!);
+            await Api.updateUserName();
+          },
         ),
         const Padding(
           padding: EdgeInsets.symmetric(vertical: 30, horizontal: 35),
@@ -123,170 +54,22 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ),
         ),
-        //.....................................
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 20,
-                  spreadRadius: 5,
-                ),
-              ],
-            ),
-            padding: const EdgeInsets.all(20),
-            height: MediaQuery.of(context).size.height * 0.15,
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  height: 70,
-                  width: 70,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/shoe1.jpg'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const Text(
-                          'HighHeals Shoes',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: blackColor,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            const Text(
-                              '1/1/22',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: greyColor,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 30,
-                              width: 30,
-                              child: Card(
-                                shape: const CircleBorder(),
-                                color: Colors.pink[600],
-                              ),
-                            ),
-                            const Text(
-                              '799 T',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: greyColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 20,
-                  spreadRadius: 5,
-                ),
-              ],
-            ),
-            padding: const EdgeInsets.all(20),
-            height: MediaQuery.of(context).size.height * 0.15,
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              children: <Widget>[
-                Container(
-                  height: 70,
-                  width: 70,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage('assets/images/shoe2.jpg'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        const Text(
-                          'HighHeals Shoes',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: blackColor,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            Text(
-                              '6/16/22',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: greyColor,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 30,
-                              width: 30,
-                              child: Card(
-                                shape: CircleBorder(),
-                                color: blackColor,
-                              ),
-                            ),
-                            Text(
-                              '1200 T',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: greyColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
+      ];
+      for (var product in boughtProducts) {
+        var newProduct = ProfileScreenBoughtProductBubble(
+          productName: product[2],
+          purchaseDate: product[9],
+          price: product[8],
+          image: product[7],
+          color: product[6],
+        );
+        children.add(newProduct);
+      }
+      return ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.zero,
+        children: children,
+      );
+    });
   }
 }
