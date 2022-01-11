a70256eb3b57e8a9800ab8f93644e835a21699fd
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-11 20:40:25 +0330

    admin checker added to view

--- view/product.php
+++ view/product.php
@@ -11,11 +11,15 @@ if ($uc->checkAuth()) {
     $pc = new ProductsController($_POST, $db);
 
     if ($pc->checkProductParams()) {
-        if (@$_POST['apiType'] == 'insert') $pc->insertProduct();
         if (@$_POST['apiType'] == 'buy') $pc->onBuy();
         if (@$_POST['apiType'] == 'select_all') $pc->selectAllProducts();
         if (@$_POST['apiType'] == 'select_by_type') $pc->selectProductsByType();
         if (@$_POST['apiType'] == 'select_by_subType') $pc->selectProductBySubtype();
         if (@$_POST['apiType'] == 'select_single') $pc->selectSingleProduct();
+
+        if ($uc->checkAdminAuth()) {
+            if (@$_POST['apiType'] == 'insert') $pc->insertProduct();
+            if (@$_POST['apiType'] == 'delete') $pc->deleteProduct();
+        }
     }
 }
