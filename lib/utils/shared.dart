1f7cc9abc3e1ae87794c6490bc21edcebd1cddea
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-11 15:29:22 +0330

    SharedPreferences file added

---
+++ lib/utils/shared.dart
@@ -0,0 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Shared {
+  static SharedPreferences? _preferences;
+  static const _userName = 'userName';
+  static const _userPassword = 'userPassword';
+  static const _userEmail = 'userEmail';
+  static const _userPhone = 'userPhone';
+  static const _userEmailOrPhone = 'userEmailOrPhone';
+  static const _likedProducts = 'likedProducts';
+
+  static Future init() async => _preferences = await SharedPreferences.getInstance();
+
+  static Future setUserName(String userName) async =>
+      await _preferences!.setString(_userName, userName);
+
+  static String? getUserName() => _preferences!.getString(_userName);
+
+  static Future setUserPassword(String password) async =>
+      await _preferences!.setString(_userPassword, password);
+
+  static String? getUserPassword() => _preferences!.getString(_userPassword);
+
+  static Future setUserEmail(String userEmail) async =>
+      await _preferences!.setString(_userEmail, userEmail);
+
+  static String? getUserEmail() => _preferences!.getString(_userEmail);
+
+  static Future setUserPhone(String userPhone) async =>
+      await _preferences!.setString(_userPhone, userPhone);
+
+  static String? getUserPhone() => _preferences!.getString(_userPhone);
+
+  static Future setUserEmailOrPhone(String userEmailOrPhone) async =>
+      await _preferences!.setString(_userEmailOrPhone, userEmailOrPhone);
+
+  static String? getUserEmailOrPhone() => _preferences!.getString(_userEmailOrPhone);
+
+  static Future setLikedProducts(List<String> likedProducts) async =>
+      await _preferences!.setStringList(_likedProducts, likedProducts);
+
+  static List<String>? getLikedProducts() => _preferences!.getStringList(_likedProducts);
+}
