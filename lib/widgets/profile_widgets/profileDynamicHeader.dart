8fdefc16f8bfba05256bc745363c85e52c20ea3b
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-15 23:38:27 +0330

    added logout for user and admin

--- lib/widgets/profile_widgets/profileDynamicHeader.dart
+++ lib/widgets/profile_widgets/profileDynamicHeader.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nbb/const.dart';
 import 'package:nbb/screens/admin.dart';
+import 'package:nbb/screens/login.dart';
 import 'package:nbb/utils/shared.dart';
 
 class ProfileDynamicHeader extends StatelessWidget {
@@ -26,20 +27,100 @@ class ProfileDynamicHeader extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          Visibility(
-            visible: adminScreen()!,
-            child: TextButton(
-              child: const Text(
-                ' Admin\nSettings',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          adminScreen()!
+              ? TextButton(
+                  child: const Text(
+                    ' Admin\nSettings',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, AdminScreen.id),
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        content: const Text(
+                          'You are about to Logout and your liked products will be deleted. Are you sure?',
+                          textAlign: TextAlign.center,
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text(
+                              'no',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          TextButton(
+                            child: const Text(
+                              'yes',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            onPressed: () {
+                              Shared.setUserName('');
+                              Shared.setLikedProducts([]);
+                              Shared.setUserPhone('');
+                              Shared.setUserEmail('');
+                              Shared.setUserPassword('');
+                              Navigator.popAndPushNamed(context, LoginScreen.id);
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                )
+              : TextButton(
+                  child: const Text(
+                    'Log Out',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        content: const Text(
+                          'You are about to Logout and your liked products will be deleted. Are you sure?',
+                          textAlign: TextAlign.center,
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text(
+                              'no',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          TextButton(
+                            child: const Text(
+                              'yes',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            onPressed: () {
+                              Shared.setUserName('');
+                              Shared.setLikedProducts([]);
+                              Shared.setUserPhone('');
+                              Shared.setUserEmail('');
+                              Shared.setUserPassword('');
+                              Navigator.popAndPushNamed(context, LoginScreen.id);
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ),
-              onPressed: () => Navigator.pushNamed(context, AdminScreen.id),
-            ),
-          ),
         ],
       ),
     );
