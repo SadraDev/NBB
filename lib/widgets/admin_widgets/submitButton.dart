3ab34741f79c17921815c456d2bb355a576445ba
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-23 18:28:03 +0330

    better submit button with loading

--- lib/widgets/admin_widgets/submitButton.dart
+++ lib/widgets/admin_widgets/submitButton.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import '../../const.dart';
 
 class SubmitButton extends StatelessWidget {
-  const SubmitButton({Key? key, this.onTap}) : super(key: key);
+  const SubmitButton({Key? key, this.onTap, this.loading}) : super(key: key);
   final void Function()? onTap;
+  final bool? loading;
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(bottom: 30),
       child: Card(
@@ -15,17 +17,24 @@ class SubmitButton extends StatelessWidget {
         shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
         color: blackColor,
         child: InkWell(
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 15),
-            child: Text(
-              'Submit',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: !loading!
+                ? const Text(
+                    'Submit',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  )
+                : Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * 0.4),
+                    child: const CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  ),
           ),
           onTap: onTap,
         ),
