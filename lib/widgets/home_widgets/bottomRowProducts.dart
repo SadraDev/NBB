04bf2244549976d5564a64f0b7862e403085ccd9
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-30 02:43:10 +0330

    show modal in bottom row products

--- lib/widgets/home_widgets/bottomRowProducts.dart
+++ lib/widgets/home_widgets/bottomRowProducts.dart
@@ -1,3 +1,4 @@
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:nbb/const.dart';
 
@@ -20,29 +21,43 @@ class BottomRowProducts extends StatelessWidget {
 }
 
 class BottomRowProductsContainer extends StatelessWidget {
-  const BottomRowProductsContainer({Key? key, this.image}) : super(key: key);
+  const BottomRowProductsContainer({Key? key, this.image, required this.modalBuilder}) : super(key: key);
   final String? image;
+  final Widget Function(BuildContext) modalBuilder;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.215,
-      height: MediaQuery.of(context).size.width * 0.215,
-      margin: const EdgeInsets.fromLTRB(15, 20, 15, 30),
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(8)),
-        color: Colors.white,
-        boxShadow: const <BoxShadow>[
-          BoxShadow(
-            color: Colors.grey,
-            blurRadius: 20,
-            offset: Offset(0, 10),
+    return GestureDetector(
+      onTap: () {
+        showMaterialModalBottomSheet(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(25),
+            ),
           ),
-        ],
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: NetworkImage(
-            '$imageURL/$image',
+          context: context,
+          builder: modalBuilder,
+        );
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.215,
+        height: MediaQuery.of(context).size.width * 0.215,
+        margin: const EdgeInsets.fromLTRB(15, 20, 15, 30),
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: Colors.white,
+          boxShadow: const <BoxShadow>[
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 20,
+              offset: Offset(0, 10),
+            ),
+          ],
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: NetworkImage(
+              '$imageURL/$image',
+            ),
           ),
         ),
       ),
