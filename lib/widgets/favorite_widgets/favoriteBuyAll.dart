830b2f02fbee911fd73747c4cbac0ca0a392d6e1
Sadra Galavani <sadra.galavani78@gmail.com>
2022-01-31 00:34:59 +0330

    favorite headers

---
+++ lib/widgets/favorite_widgets/favoriteBuyAll.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FavoriteHeader extends StatelessWidget {
+  const FavoriteHeader(this.text, {Key? key, this.buyAll}) : super(key: key);
+  final String text;
+  final void Function()? buyAll;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 35).copyWith(bottom: 20),
+      child: GestureDetector(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        onTap: buyAll,
+      ),
+    );
+  }
+}
