93dd21503fbc64bb65e6905a855c690cebbfb57d
Sadra Galavani <sadra.galavani78@gmail.com>
2022-02-04 15:34:18 +0330

    cleaner code

--- lib/utils/onLiked.dart
+++ lib/utils/onLiked.dart
@@ -7,41 +7,13 @@ class OnLiked extends ChangeNotifier {
 
   Product onLiked(Product product) {
     if (product.liked == false) {
-      product = Product(
-        id: product.id,
-        productName: product.productName,
-        productType: product.productType,
-        productSubtype: product.productSubtype,
-        minSize: product.minSize,
-        maxSize: product.maxSize,
-        price: product.price,
-        colors: product.colors,
-        image: product.image,
-        description: product.description,
-        brand: product.brand,
-        liked: true,
-      );
+      product.liked = true;
       likedProductsListId!.add(product.id.toString());
       Shared.setLikedProducts(likedProductsListId!);
-      return product;
     } else if (product.liked == true) {
       likedProductsListId!.remove(product.id.toString());
       Shared.setLikedProducts(likedProductsListId!);
-      product = Product(
-        id: product.id,
-        productName: product.productName,
-        productType: product.productType,
-        productSubtype: product.productSubtype,
-        minSize: product.minSize,
-        maxSize: product.maxSize,
-        price: product.price,
-        colors: product.colors,
-        image: product.image,
-        description: product.description,
-        brand: product.brand,
-        liked: false,
-      );
-      return product;
+      product.liked = false;
     }
     return product;
   }
